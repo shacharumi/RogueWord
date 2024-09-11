bb32a079bc7720f7bb196d57537a7da75337ecb1
shachar <shacharumi0621@gmail.com>
2024-09-11 15:59:28 +0800

    init repo

---
+++ RogueWord/RogueWord/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RogueWord
+//
+//  Created by shachar on 2024/9/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
