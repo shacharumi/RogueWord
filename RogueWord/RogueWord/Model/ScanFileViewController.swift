49a450bbcfa2b2c65f1cd1f25ae64678f5249da2
shachar <shacharumi0621@gmail.com>
2024-09-18 00:14:35 +0800

    switch branch

---
+++ RogueWord/RogueWord/Model/ScanFileViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ScanFileViewController.swift
+//  RogueWord
+//
+//  Created by shachar on 2024/9/18.
+//
+
+import Foundation
+import UIKit
+
+class ScanFileViewController: UIViewController {
+    
+}
